f0e5de08ce003c807c85663408b5d5ec52d5382c
MicziDaniel <156794173+MicziDaniel@users.noreply.github.com>
2025-02-25 22:01:52 +0100

    Add files via upload

--- Literatura.js
+++ Literatura.js
@@ -1,7 +1,23 @@
+let menuDisplay = false
+
 document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("line-button").addEventListener("click", menu)
+    let buttons = document.getElementsByClassName("line-button")
+    for(let i = 0; i < buttons.length; i++){
+        buttons[i].addEventListener("click", menu)
+    }
 });
 
 function menu(){
-    
+    let menu = document.getElementById("menu")
+    let wrap = document.getElementById("wrap")
+    if(menuDisplay){
+        menu.style.display = "none"
+        wrap.style.display = "none"
+        menuDisplay = false
+    }
+    else{
+        menu.style.display = "block"
+        wrap.style.display = "block"
+        menuDisplay = true
+    }
 }
